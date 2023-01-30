e4b15fbe311c6bed1ca11864c8723030d1db4997
JulioLovaAnjarasoa <julio@hairun-technology.com>
2023-01-30 15:56:59 +0300

    La suite de Fibonacci implemnté

---
+++ Fibonacci/includes/fibonacci.php
@@ -0,0 +1,11 @@
+<?php
+
+class Fibonacci{
+    public function suite_de_fibonacci($n)
+    {
+        if ($n <= 0) return 0;
+        if ($n== 1) return 1;
+
+        return $this->suite_de_fibonacci($n - 1) + $this->suite_de_fibonacci($n - 2);
+    }
+}
---
+++ Fibonacci/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'includes/fibonacci.php';
+
+$fibonacci = new Fibonacci();
+print_r($fibonacci->suite_de_fibonacci(4));
