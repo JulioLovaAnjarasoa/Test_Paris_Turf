926a528c1a880e7f3d7fbacaf10e789f0b0b6059
JulioLovaAnjarasoa <julio@hairun-technology.com>
2023-01-31 11:41:15 +0300

    Videotheque: delete carousel files:css - js

--- Videotheque/includes/views/accueil.php
+++ Videotheque/includes/views/accueil.php
@@ -40,8 +40,6 @@ function get_popular_movies($apiKey)
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <!-- <script src="/css/carousel.css"></script> -->
-    <link rel="stylesheet" href="./css/carousel.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
@@ -69,45 +67,7 @@ function get_popular_movies($apiKey)
             </div>
             <div class="col py-3">
                 <div class="row">
-                    <div class="container">
-                        <h1>Popular Movies</h1>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="carousel slide multi-item-carousel" id="theCarousel">
-                                    <div class="carousel-inner">
-                                        <div class="item active">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?perth,australia" class="img-responsive"></a></div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?fremantle,australia" class="img-responsive"></a></div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?west-australia" class="img-responsive"></a></div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?perth" class="img-responsive"></a></div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?quokka,perth" class="img-responsive"></a></div>
-                                        </div>
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?margaretriver,australia" class="img-responsive"></a></div>
-                                        </div>
-                                        <!-- add  more items here -->
-                                        <!-- Example item start:  -->
 
-                                        <div class="item">
-                                            <div class="col-xs-4"><a href="#1"><img src="https://source.unsplash.com/300x300/?perth,australia&r=7" class="img-responsive"></a></div>
-                                        </div>
-
-                                        <!--  Example item end -->
-                                    </div>
-                                    <a class="left carousel-control" href="#theCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-                                    <a class="right carousel-control" href="#theCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <?php
                     $popular_movies = get_popular_movies($apiKey)['results'];
                     foreach ($popular_movies as $movie) {
